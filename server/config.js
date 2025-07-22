17aa42ed3087edbaa7544ca8b235a839d5e5e51a
SANJOY <paulsanjoy2923@gmail.com>
2025-07-22 09:23:43 +0530

    /summarize added

--- server/config.js
+++ server/config.js
@@ -1,7 +1,9 @@
 const JWT_SECRET_USER = process.env.JWT_SECRET_USER
 const RAPID_API_KEY = process.env.RAPID_API_KEY
+const GEMINI_API = process.env.GEMINI_API
 
 module.exports = ({
     JWT_SECRET_USER,
-    RAPID_API_KEY
+    RAPID_API_KEY,
+    GEMINI_API
 })
--- server/route/transcript.js
+++ server/route/transcript.js
@@ -2,9 +2,15 @@ const { Router } = require("express")
 const { userMiddleware } = require("../middleware/userMiddleware")
 const axios = require("axios")
 const { chatModel } = require("../model/db")
+const { GoogleGenAI } = require("@google/genai")
+const { GEMINI_API } = require("../config")
 const { RAPID_API_KEY } = require("../config")
 
 transcriptRouter = Router()
+
+const ai = new GoogleGenAI({
+    apiKey: GEMINI_API
+});
 
 // Fetch transcript and save route
 transcriptRouter.post("/create", userMiddleware, async (req, res) => {
@@ -62,6 +68,47 @@ transcriptRouter.post("/create", userMiddleware, async (req, res) => {
         return res.status(500).json({
             message: "Failed to fetch transcript",
             error: err.message
+        })
+    }
+})
+
+// transcript summarizer
+transcriptRouter.get("/summarize/:chatId", userMiddleware, async (req, res) => {
+    const userId = req.userId
+    const chatId = req.params.chatId
+
+    const chat = await chatModel.findOne({
+        _id: chatId,
+        userId: userId
+    })
+
+    const transcript = chat.transcription
+
+    const prompt = `You are an educational assistant.
+
+                Below is a transcript from a video:
+                ---
+                ${transcript}
+                ---
+                YOU HAVE TO ONLY SUMMARIZE THE GIVEN TRANSCRIPT IN A SMALL PARAGRAPTH WHICH WILL BE SIMPLE TO UNDERSTAND AND READ AND PLEASE PLEASE EXPLAIN EVERY TOPIC EVERY WORDS RELETED TO THE SUBJECT AT LEAST GIVE DEFINATION OF THOSE TOPICS AND EXAMPLE, MINIMUM 70% OF TOTAL CHARACTERS OF TRANSCRIPT MUST BE IN THE SUMMARY, JUST ACT LIKE A TEACHER`
+
+    try{
+        const response = await ai.models.generateContent({
+            model: "gemini-2.5-flash",
+            contents: prompt
+        })
+
+        console.log(response.text)
+
+        res.json({
+            message: "Successfully generated response",
+            response: response.text
+        })
+    }catch(error){
+        console.error(error.message)
+        res.status(500).json({
+            message: "Something Went Wrong, Gemini Failed to produce summary",
+            code: 500
         })
     }
 })
